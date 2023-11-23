10d280d018a9fda048a7d886a9f3d88850c543f5
Dom <dom.benatar@hotmail.com>
2023-11-23 10:15:55 +0000

    fixed issues

---
+++ class08/src/app/blogs/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1>This is the blogs page! Cool! </h1>
+    </main>
+  );
+}
