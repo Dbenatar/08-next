62dda484bfb782e9d879c8d6beaf349eb7499049
Dom <dom.benatar@hotmail.com>
2023-11-23 09:51:40 +0000

    fixed

---
+++ class08/src/app/blogs/[slug]/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1>This is the post title </h1>
+      <p>We love content</p>
+    </main>
+  );
+}
